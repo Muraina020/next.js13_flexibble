38f89610f79dc1e5cce6394c12058a1dc8cb63b8
Hassan Ismail <email>
2023-09-02 23:05:03 +0100

    Google Auth Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
-    <div className='flex-start flex-col paddings mb-16 '>
-        <h1>Categories</h1>
-        <h1>Posts</h1>
-        <h1>Loadmore</h1>
+    <div className="flex-start flex-col paddings mb-16 ">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Loadmore</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- lib/session.ts
+++ lib/session.ts
@@ -1,51 +1,50 @@
 import { getServerSession } from "next-auth/next";
-import {NextAuthOptions, User} from "next-auth";
-import {AdapterUser} from "next-auth/adapters";
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
-import {JWT} from "next-auth/jwt";
+import { JWT } from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID!,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        // })
-        GoogleProvider({
-            clientId: '240742137801-c22kqfdf06mghfqljo2uevoog66ln5bi.apps.googleusercontent.com',
-            clientSecret: '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',
-        })
-    ],
-//     jwt: {
-//         encode: ({secret, token}) => {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // GoogleProvider({
+    //   clientId:
+    //     "240742137801-c22kqfdf06mghfqljo2uevoog66ln5bi.apps.googleusercontent.com",
+    //   clientSecret: "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",
+    // }),
+  ],
+  //     jwt: {
+  //         encode: ({secret, token}) => {
 
-//         },
-//         decode: async ({secret, token}) => {
+  //         },
+  //         decode: async ({secret, token}) => {
 
-//         }
-// },
-theme: {
+  //         }
+  // },
+  theme: {
     colorScheme: "light",
-    logo: "/logo.png"
-},
-callbacks: {
-    async session({ session }){
-      return session
+    logo: "/logo.png",
+  },
+  callbacks: {
+    async session({ session }) {
+      return session;
     },
-    async signIn({ user } : {user: AdapterUser | User}) {
-
-        try {
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
         // get the user if they exist
 
         // if they don't exist, create them
 
         // return true if they exist or were created
-         return true
-
-        } catch (error: any) {
-           console.log(error);
-           return false;
-        }
-    }
-}
-}
+        return true;
+      } catch (error: any) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
